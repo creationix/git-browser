bae4746e4da57e561d9a8a637a389a9afdfc3893
Tim Caswell <tim@creationix.com>
2013-09-06 12:36:33 -0500

    Implement web backend with canned data

--- src/main.js
+++ src/main.js
@@ -1,180 +1,186 @@
 var domBuilder = require('dombuilder');
 var ui = require('./ui.js');
-var data = require('./data.js');
+module.exports = function (backend) {
 
-ui.push(repoList());
-// var repo = data.repos[1];
-// ui.push(historyList(repo));
-// var hash = repo.db.refs["refs/heads/master"];
-// var commit = repo.db.objects[hash].body;
-// ui.push(commitPage(repo, commit));
-// var tree = repo.db.objects[commit.tree].body;
-// ui.push(filesList(repo, tree));
+  backend.getRepos(function (err, repos) {
+    if (err) throw err;
+    ui.push(repoList(repos));
+  });
 
-function onclick(handler) {
-  var args = Array.prototype.slice.call(arguments, 1);
-  return function (evt) {
-    evt.preventDefault();
-    return handler.apply(this, args);
-  };
-}
-
-function repoList() {
-  return domBuilder(["section.page", {"data-position": "current"},
-    ["header",
-      ["button", {onclick:onclick(add)}, "⊕"],
-      ["h1", "Git Repositories"]
-    ],
-    ["ul.content.header", data.repos.map(function (repo) {
-      return ["li", { href:"#", onclick: onclick(load, repo) },
-        [".icon.right", "❱"],
-        ["p", repo.name],
-        ["p", repo.description]
-      ];
-    })]
-  ]);
-
-  function load(repo) {
-    ui.push(historyList(repo));
-  }
-
-  function add() {
-    ui.push(addPage());
-  }
-}
-
-function addPage() {
-  return domBuilder(["section.page",
-    ["header",
-      ["button.back", {onclick: ui.pop}, "❰"],
-      ["h1", "Clone Repository"]
-    ],
-    ["content.header",
-      ["h2", "TODO: Implement me"]
-    ]
-  ]);
-}
-
-function historyList(repo) {
-  var list = [];
-  var $ = {};
-  var chunkSize = 9;
-  var stream = data.historyStream(repo.db, repo.db.refs["refs/heads/master"]);
-  enqueue();
-  return domBuilder(["section.page",
-    ["header",
-      ["button.back", {onclick: ui.pop}, "❰"],
-      ["h1", repo.name]
-    ],
-    ["ul.content.header", list]
-  ], $);
-
-  function enqueue() {
-    for (var i = 0; i < chunkSize; ++i) {
-      var commit = stream.next();
-      if (!commit) return;
-      var title = truncate(commit.message, 80);
-      list.push(["li", { href:"#", onclick: onclick(load, commit) },
-        [".icon.right", "❱"],
-        ["p", title],
-        ["p", commit.hash]
-      ]);
-    }
-    list.push(["li", { href:"#", onclick: onclick(more) },
-      ["p$more", "Load More..."]
-    ]);
-  }
-
-  function more() {
-    var li = $.more;
-    while (li.tagName !== "LI") li = li.parentNode;
-    var ul = li.parentNode;
-    ul.removeChild(li);
-    list = [];
-    enqueue();
-    $.more = null;
-    ul.appendChild(domBuilder(list, $));
-    return;
-  }
-
-  function load(commit) {
-    ui.push(commitPage(repo, commit));
-  }
-}
-
-function commitPage(repo, commit) {
-  var details = [];
-  var body = ["section.page",
-    ["header",
-      ["button.back", {onclick: ui.pop}, "❰"],
-      ["h1", repo.name]
-    ],
-    [".content.header", details]
-  ];
-  details.push(
-    ["header", ["h2", "message:"]],
-    ["p", {css:{whiteSpace:"pre-wrap"}}, commit.message]);
-  details.push(
-    ["header", ["h2", {css:{marginTop:0}}, "tree:"]],
-    ["button.recommend", {onclick: enter}, commit.tree]);
-  if (commit.parents) {
-    details.push(["header",
-      ["h2", {css:{marginTop:0}},
-        "parent" + (commit.parents.length === 1 ? "" : "s") + ":"
-      ]
-    ]);
-    commit.parents.forEach(function (parent) {
-      details.push(["button", {onclick: ascend(parent)}, parent]);
-    });
-  }
-  details.push(
-    ["header", ["h2", {css:{marginTop:0}}, "author:"]],
-    ["p", commit.author]);
-  if (commit.author !== commit.committer) {
-    details.push(
-      ["header", ["h2", {css:{marginTop:0}}, "committer:"]],
-      ["p", commit.committer]);
-  }
-  details.push(
-    ["header", ["h2", {css:{marginTop:0}}, "hash:"]],
-    ["button", {disabled:true}, commit.hash]);
-
-  return domBuilder(body);
-
-  function enter() {
-    var tree = repo.db.objects[commit.tree].body;
-    ui.push(filesList(repo, tree));
-  }
-
-  function ascend(parent) {
-    return function () {
-      var commit = repo.db.objects[parent].body;
-      ui.peer(commitPage(repo, commit));
+  function onclick(handler) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return function (evt) {
+      evt.preventDefault();
+      return handler.apply(this, args);
     };
   }
-}
 
-function filesList(repo, tree) {
-  return domBuilder(["section.page",
-    ["header",
-      ["button.back", {onclick: ui.pop}, "❰"],
-      ["h1", repo.name]
-    ],
-    ["ul.content.header", tree.map(function (file) {
-      return ["li", { href:"#", onclick: onclick(load, file) },
-        [".icon.right", "❱"],
-        ["p", file.name],
-        ["p", file.hash]
-      ];
-    })]
-  ]);
-  function load(file) {
-    console.log("TODO: load file");
+  function repoList(repos) {
+    return domBuilder(["section.page", {"data-position": "current"},
+      ["header",
+        ["button", {onclick:onclick(add)}, "⊕"],
+        ["h1", "Git Repositories"]
+      ],
+      ["ul.content.header", repos.map(function (repo) {
+        return ["li", { href:"#", onclick: onclick(load, repo) },
+          [".icon.right", "❱"],
+          ["p", repo.name],
+          ["p", repo.description]
+        ];
+      })]
+    ]);
+
+    function load(repo) {
+      backend.getHistoryStream(repo, function (err, stream) {
+        if (err) throw err;
+        ui.push(historyList(repo, stream));
+      });
+    }
+
+    function add() {
+      ui.push(addPage());
+    }
   }
-}
 
-function truncate(message, limit) {
-  var title = message.split(/[\r\n]/)[0];
-  if (title.length > limit) title = title.substr(0, limit - 3) + "...";
-  return title;
-}
+  function addPage() {
+    return domBuilder(["section.page",
+      ["header",
+        ["button.back", {onclick: ui.pop}, "❰"],
+        ["h1", "Clone Repository"]
+      ],
+      ["content.header",
+        ["h2", "TODO: Implement me"]
+      ]
+    ]);
+  }
+
+  function historyList(repo, stream) {
+    var list = [];
+    var $ = {};
+    var chunkSize = 9;
+    enqueue();
+    return domBuilder(["section.page",
+      ["header",
+        ["button.back", {onclick: ui.pop}, "❰"],
+        ["h1", repo.name]
+      ],
+      ["ul.content.header", list]
+    ], $);
+
+    function enqueue() {
+      for (var i = 0; i < chunkSize; ++i) {
+        var commit = stream.next();
+        if (!commit) return;
+        var title = truncate(commit.message, 80);
+        list.push(["li", { href:"#", onclick: onclick(load, commit) },
+          [".icon.right", "❱"],
+          ["p", title],
+          ["p", commit.hash]
+        ]);
+      }
+      list.push(["li", { href:"#", onclick: onclick(more) },
+        ["p$more", "Load More..."]
+      ]);
+    }
+
+    function more() {
+      var li = $.more;
+      while (li.tagName !== "LI") li = li.parentNode;
+      var ul = li.parentNode;
+      ul.removeChild(li);
+      list = [];
+      enqueue();
+      $.more = null;
+      ul.appendChild(domBuilder(list, $));
+      return;
+    }
+
+    function load(commit) {
+      ui.push(commitPage(repo, commit));
+    }
+  }
+
+  function commitPage(repo, commit) {
+    var details = [];
+    var body = ["section.page",
+      ["header",
+        ["button.back", {onclick: ui.pop}, "❰"],
+        ["h1", repo.name]
+      ],
+      [".content.header", details]
+    ];
+    details.push(
+      ["header", ["h2", "message:"]],
+      ["p", {css:{whiteSpace:"pre-wrap"}}, commit.message]);
+    details.push(
+      ["header", ["h2", {css:{marginTop:0}}, "tree:"]],
+      ["button.recommend", {onclick: enter}, commit.tree]);
+    if (commit.parents) {
+      details.push(["header",
+        ["h2", {css:{marginTop:0}},
+          "parent" + (commit.parents.length === 1 ? "" : "s") + ":"
+        ]
+      ]);
+      commit.parents.forEach(function (parent) {
+        details.push(["button", {onclick: ascend(parent)}, parent]);
+      });
+    }
+    details.push(
+      ["header", ["h2", {css:{marginTop:0}}, "author:"]],
+      ["p", commit.author]);
+    if (commit.author !== commit.committer) {
+      details.push(
+        ["header", ["h2", {css:{marginTop:0}}, "committer:"]],
+        ["p", commit.committer]);
+    }
+    details.push(
+      ["header", ["h2", {css:{marginTop:0}}, "hash:"]],
+      ["button", {disabled:true}, commit.hash]);
+
+    return domBuilder(body);
+
+    function enter() {
+      backend.getTree(repo, commit.tree, function (err, tree) {
+        if (err) throw err;
+        ui.push(filesList(repo, tree));
+      });
+    }
+
+    function ascend(parent) {
+      return function () {
+        backend.getCommit(repo, parent, function (err, commit) {
+          if (err) throw err;
+          ui.peer(commitPage(repo, commit));
+        });
+      };
+    }
+  }
+
+  function filesList(repo, tree) {
+    return domBuilder(["section.page",
+      ["header",
+        ["button.back", {onclick: ui.pop}, "❰"],
+        ["h1", repo.name]
+      ],
+      ["ul.content.header", tree.map(function (file) {
+        return ["li", { href:"#", onclick: onclick(load, file) },
+          [".icon.right", "❱"],
+          ["p", file.name],
+          ["p", file.hash]
+        ];
+      })]
+    ]);
+    function load(file) {
+      console.log("TODO: load file");
+    }
+  }
+
+  function truncate(message, limit) {
+    var title = message.split(/[\r\n]/)[0];
+    if (title.length > limit) title = title.substr(0, limit - 3) + "...";
+    return title;
+  }
+
+
+};
+
--- src/web.js
+++ src/web.js
@@ -1,1 +1,119 @@
-require('./main.js');
+
+function getRepos(callback) {
+  callback(null, [
+    {
+      name: "creationix/msgpack-js",
+      description: "The msgpack protocol implemented in pure javascript.",
+      db: require('../repos/msgpack-js.json')
+    },
+    {
+      name: "creationix/conquest",
+      description: "A remake of the classic Lords of Conquest for C64 implemented in JavaScript",
+      db: require('../repos/conquest.json')
+    },
+    {
+      name: "creationix/gen-run",
+      description: "Generator Async Runner. Makes it possible to yield and wait for callbacks and continuables.",
+      db: require('../repos/gen-run.json')
+    },
+    {
+      name: "creationix/rec",
+      description: "A tool for recording CLI programs and posting their output",
+      db: require('../repos/rec.json')
+    },
+    {
+      name: "creationix/dombuilder",
+      description: "An easy dombuilder using json-ml style syntax",
+      db: require('../repos/dombuilder.json')
+    },
+    {
+      name: "creationix/domlog",
+      description: "A simple on-screen logger using dombuilder to create elements.",
+      db: require('../repos/domlog.json')
+    },
+    {
+      name: "creationix/push-to-pull",
+      description: "Convert a push-filter to a pull-filter (for simple streams) ",
+      db: require('../repos/push-to-pull.json')
+    },
+    {
+      name: "creationix/uvrun",
+      description: "Tiny node module to expose uv_run and uv_run_once to JavaScript",
+      db: require('../repos/uvrun.json')
+    },
+    {
+      name: "creationix/mine",
+      description: "Miner digs into a javascript file looking for require calls. Used to statically extract common js dependencies.",
+      db: require('../repos/mine.json')
+    },
+  ]);
+}
+
+// Returns a stream that emits one commit at time starting with hash.
+// Commits are sorted by author date.
+function getHistoryStream(repo, callback) {
+  var db = repo.db;
+  var hash = db.refs["refs/heads/master"];
+  // queue of hashes/date pairs sorted by date
+  var queue = [];
+  // hashes we've already put in the queue
+  var seen = {};
+  var done = false;
+  if (hash) {
+    enqueue(hash);
+  }
+  else {
+    var keys = Object.keys(db.refs);
+    for (var i = 0, l = keys.length; i < l; i++) {
+      enqueue(db.refs[keys[i]]);
+    }
+  }
+
+  callback(null, { next: getNext, abort: abort });
+
+  function getNext() {
+    if (done) return;
+    var next = queue.pop();
+    if (!next) return abort();
+    next = next[0];
+    if (next.parents) {
+      next.parents.forEach(enqueue);
+    }
+    return next;
+  }
+
+  function abort() {
+    done = true;
+    queue = null;
+    seen = null;
+  }
+
+  function enqueue(hash) {
+    if (hash in seen) return;
+    var object = db.objects[hash];
+    if (!object) throw new Error("Unknown hash " + hash);
+    if (object.type !== "commit") return;
+    var commit = object.body;
+    commit.hash = hash;
+    var match = commit.author.match(/([0-9]+) ([-+]?[0-9]+)$/);
+    var timestamp = match[1];
+    if (!timestamp) throw new Error("Invalid timestamp in " + commit.author);
+    timestamp = parseInt(timestamp, 10);
+    var index = queue.length;
+    while (index > 0 && queue[index - 1][1] > timestamp) index--;
+    queue.splice(index, 0, [commit, timestamp]);
+    seen[hash] = true;
+  }
+
+}
+
+function getHash(repo, hash, callback) {
+  callback(null, repo.db.objects[hash].body);
+}
+
+require('./main.js')({
+  getRepos: getRepos,
+  getHistoryStream: getHistoryStream,
+  getCommit: getHash,
+  getTree: getHash
+});
