faac4e134f9de608d1a939eb66ba45b6eae7afd7
Tim Caswell <tim@creationix.com>
2013-09-25 14:54:44 -0500

    Update chrome example to latest 0.5.x js-git API

--- src/chrome.js
+++ src/chrome.js
@@ -1,7 +1,7 @@
 /*global FileError*/
 window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
 
-require('js-git/lib/platform.js')({
+var platform = {
   tcp: require('./chrome-tcp.js'),
   sha1: require('./sha1.js'),
   bops: require('./bops/index.js'),
@@ -10,18 +10,13 @@ require('js-git/lib/platform.js')({
   // trace: require('./trace.js'),
   trace: false,
   agent: "jsgit/" + require('js-git/package.json').version,
-});
+};
 
-var fsDb = require('js-git/lib/fs-db.js');
-var repoify = require('js-git/lib/repo.js');
+var jsGit = require('js-git')(platform);
+var gitRemote = require('git-net')(platform);
+var fsDb = require('git-fs-db')(platform);
 var gitWebfs = require('./git-webfs.js');
-var tcpProto = require('js-git/protocols/tcp.js');
-var serial = require('js-git/helpers/serial.js');
-var parallel = require('js-git/helpers/parallel.js');
-var parallelData = require('js-git/helpers/parallel-data.js');
-var serial = require('js-git/helpers/serial.js');
-var parallel = require('js-git/helpers/parallel.js');
-var parallelData = require('js-git/helpers/parallel-data.js');
+
 
 var fs;
 function getFs(callback) {
@@ -81,36 +76,24 @@ require('./main.js')({
   addRepo: function (opts, onProgress, callback) {
     getFs(function (err, fs) {
       if (err) return callback(err);
-      var path = opts.hostname + opts.pathname;
-      var description = opts.description || "git://" + path;
-      path = path.replace(/\//g, "_");
-      var repo = repoify(fsDb(fs(path), true));
+      var url = "git://" + opts.hostname + opts.pathname;
+      var description = opts.description || url;
+      var path = (opts.hostname + opts.pathname).replace(/\//g, "_");
+      var repo = jsGit(fsDb(fs(path)));
+      var remote = gitRemote(url);
       var name = opts.pathname;
       if (name[0] === "/") name = name.substr(1);
       if (name.substr(name.length - 4) === ".git") name = name.substr(0, name.length - 4);
       repo.name = name;
       repo.description = description;
-      var config = {
+
+      repo.fetch(remote, {
         includeTag: true,
         onProgress: progressParser(onProgress)
-      };
-      var connection = tcpProto(opts);
-      parallelData({
-        init: repo.init(),
-        pack: connection.fetch(config),
-      }, function (err, result) {
+      }, function (err) {
         if (err) return callback(err);
-        serial(
-          parallel(
-            repo.importRefs(result.pack.refs),
-            repo.unpack(result.pack, config)
-          ),
-          connection.close()
-        )(function (err) {
-          if (err) return callback(err);
-          onProgress("Done");
-          callback(null, repo);
-        });
+        onProgress("Done");
+        callback(null, repo);
       });
     });
   },
@@ -120,7 +103,7 @@ require('./main.js')({
       fs.readdir("/", function (err, files) {
         if (err) return callback(err);
         callback(null, files.filter(isGitDir).map(function (path) {
-          var repo = repoify(fsDb(fs(path), true));
+          var repo = jsGit(fsDb(fs(path)));
           repo.name = path;
           repo.description = path;
           return repo;
@@ -129,85 +112,17 @@ require('./main.js')({
     });
   },
   getHistoryStream: function (repo, callback) {
-    // queue of hashes/date pairs sorted by date
-    var queue = [];
-    // hashes we've already put in the queue
-    var seen = {};
-    var working = 0;
-    var cb;
-    var error;
-    var done = false;
-    repo.get("HEAD", function (err, hash) {
-      if (err) return callback(err);
-      enqueue(hash);
-      callback(null, {read: read, abort: abort});
-    });
-    function enqueue(hash) {
-      if (hash in seen) return;
-      seen[hash] = true;
-      working++;
-      repo.loadCommit(hash, function (err, commit) {
-        if (err) {
-          error = err;
-          return check();
-        }
-        commit.hash = hash;
-        var match = commit.author.match(/([0-9]+) ([\-+]?[0-9]+)$/);
-        var timestamp = match[1];
-        if (!timestamp) {
-          err = new Error("Invalid timestamp in " + commit.author);
-          return check();
-        }
-        timestamp = parseInt(timestamp, 10);
-        var index = queue.length;
-        while (index > 0 && queue[index - 1][1] > timestamp) index--;
-        queue.splice(index, 0, [commit, timestamp]);
-        check();
-      });
-    }
-    function check() {
-      if (!--working && cb) {
-        var callback = cb;
-        cb = null;
-        read(callback);
-      }
-    }
-    function read(callback) {
-      if (cb) throw new Error("Only onle read at a time");
-      if (error) {
-        var err = error;
-        error = null;
-        return callback(err);
-      }
-      if (done) return callback();
-      if (working) {
-        cb = callback;
-        return;
-      }
-      var next = queue.pop();
-      if (!next) return abort(callback);
-      next = next[0];
-      if (next.parents) {
-        next.parents.forEach(enqueue);
-      }
-      callback(null, next);
-    }
-    function abort(callback) {
-      done = true;
-      queue = null;
-      seen = null;
-      callback();
-    }
+    repo.logWalk("HEAD", callback);
   },
   getCommit: function (repo, hash, callback) {
-    repo.loadCommit(hash, function (err, commit) {
+    repo.loadAs("commit", hash, function (err, commit) {
       if (err) return callback(err);
       commit.hash = hash;
       callback(null, commit);
     });
   },
   getTree: function (repo, hash, callback) {
-    repo.loadTree(hash, function (err, tree) {
+    repo.loadAs("tree", hash, function (err, tree) {
       if (err) return callback(err);
       tree.hash = hash;
       callback(null, tree);
