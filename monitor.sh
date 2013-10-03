5b93aabd99af599435fdc893b06b70ded39cf2e1
Tim Caswell <tim@creationix.com>
2013-10-03 13:01:52 -0500

    Monitor utils folder too

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ./make $@
 while true
-  do inotifywait -e create -e delete -e modify -q -r src res node_modules
+  do inotifywait -e create -e delete -e modify -q -r src utils res node_modules
   ./make $@
 done
