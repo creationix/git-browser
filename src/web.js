8a423fc5d0e622407ffbdbb07677729da686d0fc
Tim Caswell <tim@creationix.com>
2013-11-05 11:55:21 -0600

    Ass smart reloading for cache manifest.

--- src/web.js
+++ src/web.js
@@ -1,3 +1,19 @@
+// Check if a new cache is available on page load.
+window.addEventListener('load', function(e) {
+
+  window.applicationCache.addEventListener('updateready', function(e) {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+      // Browser downloaded a new app cache.
+      // if (confirm('A new version of this site is available. Load it?')) {
+        window.location.reload();
+      // }
+    } else {
+      // Manifest didn't changed. Nothing new to server.
+    }
+  }, false);
+
+}, false);
+
 // Configure the platform
 var platform = {
   bops: require('./lib/bops'),
